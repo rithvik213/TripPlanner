920aab78f05427e63f3bdbf04c82459a8bd75813
rithvik <rithvik@rntech.org>
2024-04-14 22:47:38 -0400

    added navigation from home to new trip

--- app/src/main/java/com/example/tripplanner/HomeScreenFragment.kt
+++ app/src/main/java/com/example/tripplanner/HomeScreenFragment.kt
@@ -9,15 +9,20 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-
 class HomeScreenFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home_screen, container, false)
-        return view
+        return inflater.inflate(R.layout.fragment_home_screen, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<FloatingActionButton>(R.id.fab_add)?.setOnClickListener {
+            findNavController().navigate(R.id.action_homeScreenFragment_to_tripSearchFragment)
+        }
+    }
 }
