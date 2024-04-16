2b048f5fa51793612a0e7bf16deb238073bdf84d
rithvik213 <rithvik@rntech.org>
2024-04-16 18:12:58 -0400

    made many updates to the database to take in tripadvisor image URL, added view models for homescreen trip visuals, integrated trip cards

--- app/src/main/java/com/example/tripplanner/AppDatabase.kt
+++ app/src/main/java/com/example/tripplanner/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.tripplanner
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [Itinerary::class], version = 1)
+@Database(entities = [Itinerary::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun itineraryDao(): ItineraryDao
 }
