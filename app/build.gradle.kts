d227674d79d77ff7e4ae3a1bbdca1d25ac4e7b6e
robhannon <roberthannon2002@gmail.com>
2024-04-30 19:41:52 -0400

    saving user details and modifying string to parse

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,6 +83,11 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-location:21.2.0")
 
+    implementation("com.google.api-client:google-api-client-android:1.32.1")
+    implementation("com.google.api-client:google-api-client-gson:1.32.1")
+    implementation("com.google.apis:google-api-services-calendar:v3-rev305-1.25.0")
+
+
 
 
 
